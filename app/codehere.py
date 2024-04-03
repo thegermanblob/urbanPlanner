4acdba88175f3a5dea55d6a6f17006f6c9f00eea
TGB <germanblob@protonmail.com>
2024-04-03 11:11:39 -0400

    Add working test

--- app/codehere.py
+++ app/codehere.py
@@ -2,22 +2,23 @@ from openai import AzureOpenAI
 import os
 
 
+
+development_name:str = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME")
+search_endpoint: str = os.environ.get("AZURE_SEARCH_RESOURCE_URI")
+search_index: str = os.environ.get("AZURE_SEARCH_INDEX_NAME")
+print(development_name)
 client = AzureOpenAI(
     api_key=os.environ.get("AZURE_OPENAI_APIKEY"),
     azure_endpoint= os.environ.get("AZURE_OPENAI_RESOURCE_URI"),
     api_version="2024-02-01"
 )
 
-development_name:str = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME")
-search_endpoint: str = os.environ.get("AZURE_SEARCH_RESOURCE_URI")
-search_index: str = os.environ.get("AZURE_SEARCH_INDEX_NAME")
-
-res = client.chat.completions(
-    development_name=development_name,
-)
+# res = client.chat.completions(
+#     development_name=development_name,
+# )
 
 response = client.chat.completions.create(
-    model="gpt-35-turbo", # model = "deployment_name".
+    model=development_name,
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
